e3a7bcc3c633f9f3a9b73d97861ef15a5971e4b4
AndrZakhara <art.pruttov@gmail.com>
2018-10-05 08:59:34 +0300

    index.js: init app store, add provider

--- src/index.js
+++ src/index.js
@@ -1,5 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { Provider } from 'react-redux';
+import { store } from './store/configureStore';
 import 'semantic-ui-css/semantic.min.css';
 
 import App from './container/App';
@@ -8,9 +10,9 @@ import './index.css';
 import * as serviceWorker from './serviceWorker';
 
 ReactDOM.render(
-
-    <App />,
-
+  <Provider store={store}>
+    <App />
+  </Provider>,
   document.getElementById('root')
 );
 serviceWorker.register();
