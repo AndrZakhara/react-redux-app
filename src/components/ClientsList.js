4cdc91151373fa7a2f0f15b59bde59117b3ac89c
AndrZakhara <art.pruttov@gmail.com>
2018-10-05 10:38:14 +0300

    ClienstList.js: add class ClientsList

---
+++ src/components/ClientsList.js
@@ -0,0 +1,20 @@
+import React, { Component }from 'react';
+import ClientItem from './ClientItem';
+
+export class ClientsList extends Component {
+  render() {
+    const { filter, itemList, clickedItem, filteredList } = this.props.clientList;
+    const client = filteredList.map((item) => {
+      return(
+        <ClientItem
+          key={item.general.firstName + '_' + item.general.lastName} client={item}
+        />
+      )
+    });
+    return (
+      client
+    )
+  }
+}
+
+
