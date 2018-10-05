05df6cfc473a36bb8455e7d6ddea429b201ad9b3
AndrZakhara <art.pruttov@gmail.com>
2018-10-05 11:19:12 +0300

    ClientListSearch: fix component

--- src/components/ClientListSearch.js
+++ src/components/ClientListSearch.js
@@ -2,10 +2,7 @@ import React, { Component } from 'react';
 import { Icon } from 'semantic-ui-react';
 
 export class ClientListSearch extends Component {
-
-
   render() {
-    // console.log('aaaa: ' + this.props.filter);
     return(
       <div>
         <input type="text"
