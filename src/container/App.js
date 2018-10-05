a4e7d374ad891af0933d5454ca4a04c98c127dcd
AndrZakhara <art.pruttov@gmail.com>
2018-10-05 10:33:36 +0300

    app.jf: fix export default App

--- src/container/App.js
+++ src/container/App.js
@@ -1,9 +1,14 @@
 import React, { Component } from 'react';
+import { connect } from 'react-redux';
 import MainHeader from '../components/MainHeader';
-
+// import { Container } from 'semantic-ui-react';
+import { ClientsList } from '../components/ClientsList';
+import { ClientDetails } from '../components/ClientDetails';
+import { ClientListSearch } from '../components/ClientListSearch';
+import { setFilter, setDefaultStore } from '../actions/clientListAction';
 import './App.css';
 
-export default class App extends Component {
+export class App extends Component {
   render() {
     return (
       <div>
@@ -13,6 +18,25 @@ export default class App extends Component {
     );
   }
 }
+
+const mapStateToProps = store => {
+  console.log(store);
+  return {
+    clientDetail: store.clientDetail,
+    clientList: store.clientList,
+  }
+};
+const mapDispatchToProps = dispatch => {
+  return {
+    setFilter: filter => dispatch(setFilter(filter)),
+    setDefaultStore: filter => dispatch(setDefaultStore(filter)),
+  }
+};
+
+export default connect(
+  mapStateToProps,
+  mapDispatchToProps
+)(App);
 
 
 
--- src/index.js
+++ src/index.js
@@ -4,7 +4,7 @@ import { Provider } from 'react-redux';
 import { store } from './store/configureStore';
 import 'semantic-ui-css/semantic.min.css';
 
-import App from './container/App';
+import { App } from './container/App';
 import './index.css';
 
 import * as serviceWorker from './serviceWorker';
