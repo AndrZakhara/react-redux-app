60a9a6823122a8a65fcc3721059bd9fdf2720227
Andr Zakhar <art.pruttov@gmail.com>
2018-10-07 22:55:09 +0300

    ClientListSearch.js: add props to callback

--- src/components/ClientListSearch.js
+++ src/components/ClientListSearch.js
@@ -4,10 +4,15 @@ import { Input } from 'semantic-ui-react';
 export class ClientListSearch extends Component {
 
   render() {
-
+    console.log(this.props.itemList);
     return(
       <div className='c-clients__form'>
-        <Input icon='search' placeholder='Search...' onChange={e => this.props.setFilter(e.target.value)}  fluid/>
+        <Input 
+          icon='search' 
+          placeholder='Search...' 
+          onChange={e => this.props.setFilter(e.target.value, this.props.itemList)} 
+          fluid
+        />
       </div>
     )
   }
