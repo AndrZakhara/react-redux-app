2723fede82ff697d9726a72f8b66de887d702574
AndrZakhara <art.pruttov@gmail.com>
2018-10-08 22:57:51 +0300

    {

--- src/selectors/getFilteredItemsListSelector.js
+++ src/selectors/getFilteredItemsListSelector.js
@@ -5,7 +5,40 @@ const getItemsList = store => store.clientList.itemList;
 
 export const getFilteredItemsList = createSelector(
   [getVisibilityFilter, getItemsList],
-  (filterValue, itemList) => itemList
+  (filterValue, itemList) => {
+
+    if(itemList.lenght === 0) {
+
+      return [];
+    }
+    else {
+
+        const sortObject = (obj) => {
+        let boolean = false;
+
+        const sort = (obj) => {
+
+          for (let key in obj) {
+
+            if (typeof (obj[key]) === 'object') {
+              sort(obj[key]);
+            }
+              else {
+
+              if ((obj[key]).toLowerCase().indexOf(filterValue.toLowerCase()) !== -1) {
+                boolean = true;
+              }
+            }
+          }
+          return boolean;
+        };
+        sort(obj);
+
+        return boolean;
+      };
+      return itemList.filter(obj => sortObject(obj));
+    }
+  }
 );
 
 // function filterUserList(arr, value) {
