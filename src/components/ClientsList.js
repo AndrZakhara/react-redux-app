2bcdbbfd7e519d2c7fd81203d204224b2b61da53
AndrZakhara <art.pruttov@gmail.com>
2018-10-09 12:49:50 +0300

    ClientsList.js: add proptypes

--- src/components/ClientsList.js
+++ src/components/ClientsList.js
@@ -1,9 +1,11 @@
 import React, { Component }from 'react';
 import ClientItem from './ClientItem';
+import PropTypes from 'prop-types';
 
 export class ClientsList extends Component {
 
   render() {
+
     const { filteredList, isFetching } = this.props.clientList;
     const client = filteredList.map((item) => {
 
@@ -29,3 +31,11 @@ export class ClientsList extends Component {
     }
   }
 }
+
+ClientsList.propTypes = {
+  clientList: PropTypes.shape({
+    filteredList: PropTypes.array.isRequired,
+    isFetching: PropTypes.bool.isRequired
+  }).isRequired,
+  setActiveItem: PropTypes.func.isRequired,
+};
--- src/container/App.js
+++ src/container/App.js
@@ -35,7 +35,6 @@ class App extends Component {
             <ClientsList
               clientList={clientList}
               setActiveItem={setActiveItem}
-              isFetching={clientList.isFetching}
             />
           </div>
           <ClientDetails
