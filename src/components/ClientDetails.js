a4c4b146a8961328cb467d3675299ac2305c49f4
AndrZakhara <art.pruttov@gmail.com>
2018-10-09 11:41:32 +0300

    ClientDetail.js: add proptypes

--- src/components/ClientDetails.js
+++ src/components/ClientDetails.js
@@ -1,8 +1,10 @@
 import React, { Component } from 'react';
 import { Header } from 'semantic-ui-react';
+import PropTypes from 'prop-types';
 
 export class ClientDetails extends Component {
   render() {
+    console.log(this.props);
     const { address, contact, job, general } = this.props.clientInfo.selectedItemDetail;
 
     if(
@@ -14,7 +16,8 @@ export class ClientDetails extends Component {
           <Header >To see detailed information, select client.</Header>
         </div>
       )
-    } else {
+    }
+    else {
       return (
         <div className="c-client-detail">
           <img className="c-client-detail__avatar" src={general.avatar} alt="img" />
@@ -24,12 +27,42 @@ export class ClientDetails extends Component {
             <h4>Address:</h4>
             <p>{address.street} {address.city}, {address.zipCode} {address.country}</p>
             <h4>Contact:</h4>
-            <p>Phone: {contact.phone}</p>
-            <p>email: {contact.email}</p>
+            <p>Phone: {contact.phone || 'information not found'}</p>
+            <p>email: {contact.email || 'information not found'}</p>
           </div>
         </div>
       )
     }
-
   }
 }
+
+ClientDetails.propTypes = {
+  clientInfo: PropTypes.shape({
+
+    selectedItemDetail: PropTypes.shape({
+
+      address: PropTypes.shape({
+        street: PropTypes.string.isRequired,
+        city: PropTypes.string.isRequired,
+        zipCode: PropTypes.string.isRequired,
+        country: PropTypes.string.isRequired
+      }),
+
+      contact: PropTypes.shape({
+        phone: PropTypes.string.isRequired,
+        email: PropTypes.string.isRequired
+      }),
+
+      job: PropTypes.shape({
+        title: PropTypes.string.isRequired,
+        company: PropTypes.string.isRequired
+      }),
+
+      general: PropTypes.shape({
+        firstName: PropTypes.string.isRequired,
+        lastName: PropTypes.string.isRequired,
+        avatar: PropTypes.string.isRequired
+      })
+    })
+  })
+};
